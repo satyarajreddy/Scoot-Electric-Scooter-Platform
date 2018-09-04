7b767a256082b2df6ca5a69ea26c4344f35c0f47
Mukul <mukul.ishwar23@gmail.com>
2018-09-04 06:26:35 -0700

    login added

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var Rider = require("./models/rider");
 //connect mongoose to mongodb
 mongoose.connect("mongodb://localhost:27017/scootdb",{ useNewUrlParser: true } );
+
+
+//auth
+app.use(require("express-session")({
+    secret:"Hello World",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(Rider.authenticate()));
+passport.serializeUser(Rider.serializeUser());
+passport.deserializeUser(Rider.deserializeUser());
 
 
 
@@ -43,14 +59,43 @@ app.get("/pricing", function(req,res){
     res.render("pricing");
 });
 
-app.get("/signupform", function(req,res){
-    res.render("contact");
+app.get("/register", function(req,res){
+    res.render("register");
 });
 
 
+//register route
+app.post("/register", function(req,res){
+    var newRider = new Rider({username:req.body.username});
+    Rider.register(newRider , req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("register");
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/");
+            })
+        }
+    })
+});
 
+app.get("/login", function(req,res){
+    res.render("login");
+});
 
+app.post("/login", passport.authenticate("local",
+{
+    successRedirect: "/",
+    failureRedirect:"/login"
+}),  function(req,res){
+    // empty callback no need
+});
 
+//logout
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
 
 
 
