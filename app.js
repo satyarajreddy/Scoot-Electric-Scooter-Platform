d10f1de0af73bac3fbbba7af3cc2f9d3bc88f344
Satyaraj_Reddy <nandisatyarajreddy@gmail.com>
2018-09-24 15:32:45 +0530

    sdnasbsndbfsdfs

--- app.js
+++ app.js
@@ -117,6 +117,14 @@ app.get("/calculator", function (req, res) {
     res.render("calculator");
 });
 
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.flash("error", "You need to be logged in to do that!");
+    res.redirect("/login");
+
+}
 
 
 app.get("/newprofile", function (req, res) {
